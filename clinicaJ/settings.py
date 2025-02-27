9535d9839dce3a4de9a13ffc3f5b42b9a93682ba
gilmarlobo <gilmarbassstrong@gmail.com>
2025-02-26 21:22:06 -0300

    segundo commit

--- clinicaJ/settings.py
+++ clinicaJ/settings.py
@@ -1,23 +1,26 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse
 import pymysql
 
 pymysql.install_as_MySQLdb()
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&g8u+7l2jzr(b2s7i8&0^t=#fh(u8+70t$mf!*(hk!h7&y2jj&'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&g8u+7l2jzr(b2s7i8&0^t=#fh(u8+70t$mf!*(hk!h7&y2jj&') #Adicionado valor default para caso não ache a variavel localmente.
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False # Modificado para False na produção.
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['seu-dominio.vercel.app'] #Adicione seu dominio ou o dominio da vercel.
 
 # Application definition
 
@@ -62,28 +65,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'clinicaJ.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DB_Clinica',
-        'USER': 'root',
-        'PASSWORD': 'gilm4150',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
     }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -99,10 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -111,13 +104,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
